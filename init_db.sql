294d0decfb42a5f5aa1486b0fab3aaf04b05a806
NGhazaryan2004 <narekghazaryan0304@gmail.com>
2025-12-31 19:12:14 +0400

    init db adding

--- init_db.sql
+++ init_db.sql
@@ -1,6 +1,5 @@
 CREATE DATABASE geography_db;
 CREATE USER geo_admin WITH PASSWORD 'geo_pass';
 GRANT ALL PRIVILEGES ON DATABASE geography_db TO geo_admin;
-
 \c geography_db;
 CREATE EXTENSION IF NOT EXISTS pg_trgm;
